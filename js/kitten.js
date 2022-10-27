87c19b0673e4cb7a16c2d67a329e20820f4472c4
nanodotbot <nanodotbot@gmail.com>
2022-10-27 15:10:56 +0200

    node summary

---
+++ js/kitten.js
@@ -0,0 +1,7 @@
+let noun1 = 'kitten';
+let adjective = 'silly';
+let noun2 = 'kitty';
+let verb = 'sing';
+let noun3 = 'fish';
+
+console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
